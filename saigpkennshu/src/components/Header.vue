1b19b4322b17aa63dfab8862c5f33a257aebef2b
zhangwenjing <zhangwenjing@ia.com>
2025-06-19 14:22:45 +0900

    123

---
+++ saigpkennshu/src/components/Header.vue
@@ -0,0 +1,19 @@
+<<template>
+  <header class="global-header">
+    <div class="header-inner">
+      <h1 class="logo">My App</h1>
+      <router-link v-if="isLoggedIn" to="/logout" class="logout-link">ログアウト</router-link>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  computed: {
+    isLoggedIn() {
+      return localStorage.getItem('loggedIn') === 'true'
+    }
+  }
+}
+</script>
