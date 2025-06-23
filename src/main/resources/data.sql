84b0fe9ff026c94c5b020a67f36882adbad8106e
matsura <matsura.co.jp>
2025-06-23 10:14:42 +0900

    push

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,7 +2,7 @@ INSERT INTO users (id, username,password, email,category_id) VALUES
 (1, 'aaa', 'aaa', 'mail',0);
 
 INSERT INTO request (id,paid,early,absence,late,paid_app,early_app, absence_app,late_app) VALUES 
-(1,2,3,4,5,6,7,8,9);
+(1,1,1,1,1,1,1,1,1);
 
 
 INSERT INTO attendance (id,start_time,end_time,date,userid,requestid) VALUES 
